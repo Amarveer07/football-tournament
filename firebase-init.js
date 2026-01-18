166a5439c01839af2fe8f4313227dd4fd64aeab5
Amarveer07 <amarveer07@icloud.com>
2026-01-18 19:10:26 +0000

    firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDflqJLfd9wK4eicO9Ir9NCyUZYMRIXUwI",
+  authDomain: "football-tournament-87366.firebaseapp.com",
+  databaseURL: "https://football-tournament-87366-default-rtdb.firebaseio.com",
+  projectId: "football-tournament-87366",
+  storageBucket: "football-tournament-87366.firebasestorage.app",
+  messagingSenderId: "25593487396",
+  appId: "1:25593487396:web:fec9543a7f8f41134ab8b1"
+};
+// 2) Initialize Firebase + Database
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.database();
